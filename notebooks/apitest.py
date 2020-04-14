612432c4f0ec17e010716d3ef83a55bbd905cea3
Hanneseh <he008@hdm-stuttgart.de>
2020-04-14 16:12:50 +0200

    API Queries are done, cleaning script started

--- notebooks/apitest.py
+++ notebooks/apitest.py
@@ -210,3 +210,50 @@ dfXN
 
 # export to csv
 dfXN.to_csv('/Users/Hannes/baseball-project/baseballmd/notebooks/playerStats.csv', index = False)
+
+
+# further experimentation: 
+
+personIds = "645277,593428,544369"
+splitParams = {'personIds':personIds, 'hydrate':'stats(type=[statSplits],sitCodes=[h,a,d,n,g,t,1,2,3,4,5,6,7,8,9,10,11,12,preas,posas,vr,vl,r0,r1,r2,r3,r12,r13,r23,r123,risp,o0,o1,o2,i01,i02,i03,i04,i05,i06,i07,i08,i09,ix,b2,b3,b4,b4,b5,b6,lo,lc,ac,bc],season=2019'}
+people = statsapi.get('people',params)
+people
+
+for person in people['people']:
+    print('{}'.format(person['fullName']))
+    for stat in person['stats']:
+        if len(stat['splits']): print('  {}'.format(stat['group']['displayName']))
+        for split in stat['splits']:
+            print('    {} {}:'.format(split['season'], split['split']['description']))
+            for split_stat,split_stat_value in split['stat'].items():
+                print('      {}: {}'.format(split_stat, split_stat_value))
+            print('\n')
+
+
+
+# sample url splits of season 2018 with random player IDs
+https://statsapi.mlb.com/api/v1/people?personIds=434378,519203&hydrate=stats(group=[hitting,pitching],type=[statSplits],sitCodes=[vr,vl],season=2018)
+
+# sample url splits of season 2019 with multiple players
+https://statsapi.mlb.com/api/v1/people?personIds=645277,593428,544369&hydrate=stats(type=[statSplits],sitCodes=[h,a,d,n,g,t,1,2,3,4,5,6,7,8,9,10,11,12,preas,posas,vr,vl,r0,r1,r2,r3,r12,r13,r23,r123,risp,o0,o1,o2,i01,i02,i03,i04,i05,i06,i07,i08,i09,ix,b2,b3,b4,b4,b5,b6,lo,lc,ac,bc],season=2019)
+
+# sample url splits of season 2019 only xander
+https://statsapi.mlb.com/api/v1/people?personIds=593428&hydrate=stats(type=[statSplits],sitCodes=[h,a,d,n,g,t,1,2,3,4,5,6,7,8,9,10,11,12,preas,posas,vr,vl,r0,r1,r2,r3,r12,r13,r23,r123,risp,o0,o1,o2,i01,i02,i03,i04,i05,i06,i07,i08,i09,ix,b2,b3,b4,b4,b5,b6,lo,lc,ac,bc],season=2019)
+
+# sample url splits of entire season 2019 only xander (last row in table)
+https://statsapi.mlb.com/api/v1/people?personIds=593428&hydrate=stats(type=[season],season=2018)
+
+################################# Careere Data #####################################
+# sample URL for Career data of just one specific season with one player
+https://statsapi.mlb.com/api/v1/people?personIds=593428&hydrate=stats(group=[hitting,pitching],type=[statsSingleSeason],season=2018)
+
+# sample URL for Career data of one season for several players
+https://statsapi.mlb.com/api/v1/people?personIds=645277,593428&hydrate=stats(group=[hitting,pitching],type=[statsSingleSeason],season=2018)
+
+# sample URL for Career data of entire mlb Career
+https://statsapi.mlb.com/api/v1/people?personIds=593428&hydrate=stats(group=[hitting,pitching],type=[career])
+
+statsapi.meta('baseballStats')
+
+## experimentation
+https://statsapi.mlb.com/api/v1/people?personIds=645277,593428,544369&hydrate=stats(group=[hitting,pitching,fielding],type=[yearByYear])
--- src/data/querryAPI.py
+++ src/data/querryAPI.py
@@ -1,30 +1,38 @@
-# importing the api wrapper
+###### Requests to get all data of one player (The requests work for multiple player IDs at the same time) ########
+
+# CAREER DATA: all rows, except the summery rows for the "MLB Career" in hiting and fielding 
+# https://statsapi.mlb.com/api/v1/people?personIds=593428&hydrate=stats(group=[hitting,fielding,pitching],type=[yearByYear])
+
+# CAREER DATA: the summery rows "MLB Career" in hitting and fielding: Remaining Question: How to get minor Carrer stats? Is it relevant?
+# https://statsapi.mlb.com/api/v1/people?personIds=593428&hydrate=stats(group=[hitting,fielding,pitching],type=[career])
+
+# SPLITS DATA: All the rows of the splits data table on the website, except the last row (Season summary) for one season
+# https://statsapi.mlb.com/api/v1/people?personIds=593428&hydrate=stats(type=[statSplits],sitCodes=[h,a,d,n,g,t,1,2,3,4,5,6,7,8,9,10,11,12,preas,posas,vr,vl,r0,r1,r2,r3,r12,r13,r23,r123,risp,o0,o1,o2,i01,i02,i03,i04,i05,i06,i07,i08,i09,ix,b2,b3,b4,b4,b5,b6,lo,lc,ac,bc],season=2019)
+# request must be repeated for all relevant seasons
+
+# SPLITS DATA: Season summary row for one season
+# https://statsapi.mlb.com/api/v1/people?personIds=593428&hydrate=stats(type=[season],season=2019)
+# request must be repeated for all relevant seasons
+
+###### Start of the algorithm to get all the data from the API in a database ########
+
+# imports
 import statsapi
 import pandas as pd
 
-# player IDs df
+# getting Names and IDs of the player (For now only a sample dataframe, later this will come from the database)
 players = pd.DataFrame(data={'Name': ['Ozzie Albies', 'Xander Bogaerts', 'Didi Gregorius'], 'ID':[645277, 593428, 544369]})
 players
 
-statsapi.player_stats(next(
-        x['id'] for x in statsapi.get(
-            'sports_players',
-            {'season':2019,'gameType':'R'}
-            )
-            ['people'] if x['fullName']=='Xander Bogaerts'),
-             'hitting', 'career')
+# transforming the IDs to a comma seperated string in order to serve as a parameter later on
+playerIDs = ""
+for playerID in players['ID']:
+    playerIDs = playerIDs + str(playerID) + ","
+playerIDs
 
-statsapi.get('sports_players',{'season':2019,'gameType':'R'})['people'] if x['id']==645277)'hitting', 'career')
-
-statsapi.get('sports_players',{'season':2019,'gameType':'R'})
-
-statsapi.get('people',{'personIds':'645277,593428','hydrate':'stats(group=[hitting,pitching],type=[statSplits],sitCodes=[vr,vl])')
-
-requests.get
-
-personIds = "645277,593428,544369"
-splitParams = {'personIds':personIds, 'hydrate':'stats(type=[statSplits],sitCodes=[h,a,d,n,g,t,1,2,3,4,5,6,7,8,9,10,11,12,preas,posas,vr,vl,r0,r1,r2,r3,r12,r13,r23,r123,risp,o0,o1,o2,i01,i02,i03,i04,i05,i06,i07,i08,i09,ix,b2,b3,b4,b4,b5,b6,lo,lc,ac,bc,h0],season=2019'}
-people = statsapi.get('people',params)
+# sample request and printing of the data 
+splitParams = {'personIds':playerIDs, 'hydrate':'stats(type=[statSplits],sitCodes=[h,a,d,n,g,t,1,2,3,4,5,6,7,8,9,10,11,12,preas,posas,vr,vl,r0,r1,r2,r3,r12,r13,r23,r123,risp,o0,o1,o2,i01,i02,i03,i04,i05,i06,i07,i08,i09,ix,b2,b3,b4,b4,b5,b6,lo,lc,ac,bc],season=2019)'}
+people = statsapi.get('people',splitParams)
 people
 
 for person in people['people']:
@@ -37,16 +45,13 @@ for person in people['people']:
                 print('      {}: {}'.format(split_stat, split_stat_value))
             print('\n')
 
+# retrieving all career data
+careerParams = {'personIds':playerIDs, 'hydrate':'stats(group=[hitting,fielding,pitching],type=[yearByYear])'}
+careerStats = statsapi.get('people',careerParams)
+careerStats
 
+df = pd.DataFrame.from_dict(careerStats)
+df
 
-# sample url splits of season 2018 with random player IDs
-https://statsapi.mlb.com/api/v1/people?personIds=434378,519203&hydrate=stats(group=[hitting,pitching],type=[statSplits],sitCodes=[vr,vl],season=2018)
-
-# sample url splits of season 2019 with multiple players
-https://statsapi.mlb.com/api/v1/people?personIds=645277,593428,544369&hydrate=stats(type=[statSplits],sitCodes=[h,a,d,n,g,t,1,2,3,4,5,6,7,8,9,10,11,12,preas,posas,vr,vl,r0,r1,r2,r3,r12,r13,r23,r123,risp,o0,o1,o2,i01,i02,i03,i04,i05,i06,i07,i08,i09,ix,b2,b3,b4,b4,b5,b6,lo,lc,ac,bc,h0],season=2019)
-
-# sample url splits of season 2019 only xander
-https://statsapi.mlb.com/api/v1/people?personIds=593428&hydrate=stats(type=[statSplits],sitCodes=[h,a,d,n,g,t,1,2,3,4,5,6,7,8,9,10,11,12,preas,posas,vr,vl,r0,r1,r2,r3,r12,r13,r23,r123,risp,o0,o1,o2,i01,i02,i03,i04,i05,i06,i07,i08,i09,ix,b2,b3,b4,b4,b5,b6,lo,lc,ac,bc,h0],season=2019)
-
-
-### sample URL for Career data 
+for player in careerStats['people']:
+    print('{}'.format(player['fullName']))
