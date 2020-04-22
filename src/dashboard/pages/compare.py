5f5ba365dacfce442bd59cdeb957cb0443df455f
August Carlsson <Carlsson@Augusts-MacBook-Pro.local>
2020-04-22 18:00:34 +0200

    tabletry

--- src/dashboard/pages/compare.py
+++ src/dashboard/pages/compare.py
@@ -4,9 +4,11 @@ from dash.dependencies import Input, Output
 import pandas as pd
 from app import app
 from .components import Header
-import dash_table
+import dash_table as dt 
 import plotly.express as px
-df3= pd.read_csv('/Users/Hannes/baseball-project/baseballmd/src/dashboard/pages/datacsv/notebooks_playerStats.csv')
+from pymongo import MongoClient
+
+import numpy as np 
 
 '''
 TO DO:
@@ -14,65 +16,46 @@ TO DO:
     - Hints: {"type" : "career", "statGroupe" : "hitting"}
 - Try to figure a way out 
 '''
+client = MongoClient("localhost:27017")
+db=client['baseballmd']
+collection = db.careerStats
 
+rawCompareData = pd.DataFrame(list(collection.find({"type" : "career", "statGroupe" : "hitting"})))
 #https://dash.plotly.com/dash-core-components/graph
-
-df4 = pd.DataFrame(dict(
-    r=[914, 542, 228, 14, 107],
-    theta=['Games played','Runs','doubles',
-           'triples', 'Homeruns']))
-
-df5 = pd.DataFrame(dict(
-    r=[375, 241, 92, 18, 54],
-    theta=['Games played','Runs','doubles',
-           'triples', 'Homeruns']))
-
-#df6 = 
-    #r=df3[],
-    #theta=['Games played','Runs','doubles',
-           #'triples', 'Homeruns']))
+selectedCompareData=rawCompareData.drop(columns=['_id', 'id', 'sport','gameType'])
+selectedCompareData.columns 
+# selectedCompareData.rename(columns={'fullName', 'type', 'statGroupe', 'gamesPlayed', 'groundOuts',
+#        'airOuts', 'runs', 'doubles', 'triples', 'homeRuns', 'strikeOuts',
+#        'baseOnBalls', 'intentionalWalks', 'hits', 'hitByPitch', 'avg',
+#        'atBats', 'obp', 'slg', 'ops', 'caughtStealing', 'stolenBases',
+#        'stolenBasePercentage', 'groundIntoDoublePlay', 'numberOfPitches',
+#        'plateAppearances', 'totalBases', 'rbi', 'leftOnBase', 'sacBunts',
+#        'sacFlies', 'babip', 'groundOutsToAirouts', 'atBatsPerHomeRun',
+#        'gameType'}
 
 
-fig = px.line_polar(df4, r='r', theta='theta', line_close=True, width=500, height=500)
-fig2 = px.line_polar(df5, r='r', theta='theta', line_close=True, width=500, height=500)
+
+
+df = pd.DataFrame({"A": [1, 2, 3], "B": [4, 5, 6]})
+df.rename(columns={"A": "a", "B": "c"})
+
+
 layout = html.Div([
     Header(),
     html.H3('Compare Players'),
 
-
-    # dash_table.DataTable(
-    #     id='datatable-interactivity',
-    #     columns=[
-    #         {"name": i, "id": i, "deletable": True, "selectable": True} for i in df3.columns
-    #     ],
-    #     data=df3.to_dict('records'),
-    #     #style_cell={'padding': '5px',
-    #      #               'whiteSpace': 'no-wrap',
-    #       #              'overflow': 'hidden',
-    #        #             
-    #         #            'maxWidth': 0,
-    #          #           'height': 30,
-    #           #          'textAlign': 'left'},
+    html.Div([dt.DataTable(
+        id='table',
+        columns=[{"name": i, "id": i} for i in selectedCompareData.columns],
+        data=selectedCompareData.to_dict('records'),
+        style_table={'overflowX': 'scroll', 'height':'auto'}
+    )])
+    # dt.DataTable(
+    #     id='table', 
+        
+    #     data=rawPlayerData.to_dict('records'),
     #     style_table={'overflowX': 'scroll', 'whiteSpace': 'normal', 'height':'auto'},
-    #     editable=True,
-    #     filter_action="native",
-    #     sort_action="native",
-    #     sort_mode="multi",
-    #     column_selectable="single",
-    #     row_selectable="multi",
-    #     row_deletable=True,
-    #     selected_columns=[],
-    #     selected_rows=[],
-    #     page_action="native",
-    #     page_current= 0,
-    #     page_size= 10,
-    # ),
-    # html.Div(id='datatable-interactivity-container'),
-
-    # html.Div([dcc.Graph(figure=fig)], style={'float':'left'}),
-    # html.Div([dcc.Graph(figure=fig2)], style={'float':'right'})
-    
-
-
+        
+    # )
 
 ], className="page")
